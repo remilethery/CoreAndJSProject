076424e8bc18c3ff3a91b780baa49e37583eca3f
Remi <remi.lethery@gmail.com>
2020-01-11 18:03:29 +0100

    Add ProductDetails class

---
+++ SpyStore.Models/Entities/Base/ProductDetails.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+public class ProductDetails
+{
+    [MaxLength(3800)]
+    public string Description { get; set; }
+
+    [MaxLength(50)]
+    public string ModelNumber { get; set; }
+
+    [MaxLength(50)]
+    public string ModelName { get; set; }
+
+    [MaxLength(150)]
+    public string ProductImage { get; set; }
+
+    [MaxLength(150)]
+    public string ProductImageLarge { get; set; }
+
+    [MaxLength(150)]
+    public string ProductImageThumb { get; set; }
+
+}
