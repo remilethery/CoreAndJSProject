f8609a69c3a745fa2e7bcd3fc5b9a1118d1d6811
Remi <remi.lethery@gmail.com>
2020-01-11 16:22:18 +0100

    Add Customer Model and reference to Automapper and NewtonsoftJson

--- SpyStore.Dal/EFStructures/StoreContext.cs
+++ SpyStore.Dal/EFStructures/StoreContext.cs
@@ -11,10 +11,15 @@ public class StoreContext: DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasIndex(e =>
+           e.EmailAddress).HasName("IX_Customers").IsUnique();
+        });
     }
 }
