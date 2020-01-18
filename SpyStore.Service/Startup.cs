55c4881d98b3d5dec51e06bddc484d9e35467a5f
Remi <remi.lethery@gmail.com>
2020-01-18 18:55:19 +0100

    Add Startup configuration for REST API

--- SpyStore.Service/Startup.cs
+++ SpyStore.Service/Startup.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SpyStore.Dal.EFStructures;
+using SpyStore.Dal.Initialization;
+using SpyStore.Dal.Repos;
+using SpyStore.Dal.Repos.Interfaces;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SpyStore.Service
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IHostingEnvironment _env;
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
+            _env = env;
             Configuration = configuration;
         }
 
@@ -25,6 +38,46 @@ namespace SpyStore.Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMvcCore()
+                .AddJsonFormatters(j =>
+                {
+                    j.ContractResolver = new DefaultContractResolver();
+                    j.Formatting = Formatting.Indented;
+                });
+
+            // http://docs.asp.net/en/latest/security/cors.html
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials();
+                });
+            });
+            //NOTE: Did not disable mixed mode running here
+            services.AddDbContextPool<StoreContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("SpyStore")));
+            services.AddScoped<ICategoryRepo, CategoryRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IShoppingCartRepo, ShoppingCartRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderDetailRepo, OrderDetailRepo>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "SpyStore Service",
+                        Version = "v1",
+                        Description = "Service to support the SpyStore sample eCommerce site",
+                        TermsOfService = new System.Uri("http://localhost:23741/LICENSE.txt")
+                    });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory,xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,8 +86,21 @@ namespace SpyStore.Service
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = serviceScope.ServiceProvider.GetRequiredService<StoreContext>();
+                    SampleDataInitializer.InializeData(context);
+                }
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","SpyStore Service v1");
+                //c.RoutePrefix = string.Empty;
+            });
+            app.UseStaticFiles();
+            app.UseCors("AllowAll");  // has to go before UseMvc
             app.UseMvc();
         }
     }
